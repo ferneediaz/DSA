70c9269ed57650cd793df99054662f1dfe8339c5
ferneediaz <dandiaz10@yahoo.com>
2024-06-12 12:10:14 +0800

    fixed bugs

--- ransomnote.py
+++ ransomnote.py
@@ -1,14 +1,4 @@
-<<<<<<< HEAD
-class Solution:
-    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        ransomNote = list(ransomNote)
-        magazine = list(magazine)
-        for char in ransomNote:
-            if char in magazine:
-                magazine.remove(char)
-            else:
-                return Falsse
-=======
+
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         ransomNote = list(ransomNote)
@@ -18,5 +8,13 @@ class Solution:
                 magazine.remove(char)
             else:
                 return False
->>>>>>> origin/main
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        ransomNote = list(ransomNote)
+        magazine = list(magazine)
+        for char in ransomNote:
+            if char in magazine:
+                magazine.remove(char)
+            else:
+                return False
         return True
