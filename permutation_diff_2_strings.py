44ec7845af79fc21dbd92006ea0bd504f9014de1
ferneediaz <dandiaz10@yahoo.com>
2024-07-04 00:01:50 +0800

    updatged the answer for find permutation of two string better readability

--- permutation_diff_2_strings.py
+++ permutation_diff_2_strings.py
@@ -1,13 +1,10 @@
 class Solution:
     def findPermutationDifference(self, s: str, t: str) -> int:
-        ht: dict[str, int] = dict()
-        for i in range(len(s)):
-            if s[i] in ht:
-                ht[s[i]] = abs(ht[s[i]] - i)
-            else:
-                ht[s[i]] = i
-            if t[i] in ht:
-                ht[t[i]] = abs(ht[t[i]] - i)
-            else:
-                ht[t[i]] = i
-        return sum(ht.values())
+        total = 0
+
+        for index,c in enumerate(s):
+            index2 = t.index(c)
+
+            total += abs(index2-index)
+
+        return total
