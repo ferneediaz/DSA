7715f3dd352b6b5de4fe388f2de7bdf93cde341e
ferneediaz <dandiaz10@yahoo.com>
2024-10-15 20:49:32 +0800

    countBy kyu8 codewars

---
+++ countBy.go
@@ -0,0 +1,12 @@
+package kata
+
+
+func CountBy(x, n int) []int {
+  result := make([]int,n )
+  for i := 1; i <= n; i++ {
+        result[i-1] = x * i
+    }
+    
+    // Return the slice containing the multiples
+    return result
+}
